14125ed18d5b4d2c96344a7f7bab3bafea5e344e
Pilyugin Ivan <rect1791@gmail.com>
2017-12-16 23:20:28 +0300

    add save button in toolbar

--- app/src/main/java/bobrovskaya/rect12/dreamdiary/CreateDreamActivity.java
+++ app/src/main/java/bobrovskaya/rect12/dreamdiary/CreateDreamActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 
@@ -32,6 +33,27 @@ public class CreateDreamActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dream_create, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add_db_dream) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 //    private insertDream() {
 //
