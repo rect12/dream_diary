9a2f7b25930cc1c3d3a5a347278b0b50bf9b9312
Pilyugin Ivan <rect1791@gmail.com>
2017-12-17 19:10:58 +0300

    fix bug with white status bar

--- app/src/main/java/bobrovskaya/rect12/dreamdiary/SettingsActivity.java
+++ app/src/main/java/bobrovskaya/rect12/dreamdiary/SettingsActivity.java
@@ -1,7 +1,9 @@
 package bobrovskaya.rect12.dreamdiary;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by rect on 12/2/17.
@@ -12,5 +14,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_create_dream);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
 }
