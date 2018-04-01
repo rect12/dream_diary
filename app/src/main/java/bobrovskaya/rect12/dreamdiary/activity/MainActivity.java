195144efd018b5695ef735b48d520caf96d86400
Pilyugin Ivan <rect1791@gmail.com>
2018-04-01 22:12:39 +0300

    fix toolbar

--- app/src/main/java/bobrovskaya/rect12/dreamdiary/activity/MainActivity.java
+++ app/src/main/java/bobrovskaya/rect12/dreamdiary/activity/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
 //        toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -70,7 +71,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+//        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -82,9 +83,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_search) {
-            return true;
-        }
+//        if (id == R.id.action_search) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
